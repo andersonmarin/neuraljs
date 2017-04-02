9b2a2b21c055e78e1b5f486a68f844fdab44eb9c
Anderson Marin <anderson@gflexdev.com.br>
2017-04-02 19:26:20 -0300

    Ajuste na estrutura de diretorios do projeto

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-var neural = require('./neural');
+var neural = require('./lib/neural');
 
-// classificando uma entrada como ave
-// bico, olhos, penas, bipede, asas, voa
-var dados_treino = [
-  {
-    // pombo
-    input: [1, 1, 1, 1, 1, 1],
-    output: true
-  },
-  {
-    // galinha
-    input: [1, 1, 1, 1, 1, 0],
-    output: true
-  },
-  {
-    // ornitorrinco
-    input: [1, 1, 0, 0, 0, 0],
-    output: false
-  }
-];
-
-// neuronios da camada inicial
-var a = new neural.Neuronio();
-var b = new neural.Neuronio();
-var c = new neural.Neuronio();
-
-// habilitando o modo debug para ver os logs
-neural.debug = true;
-
-// executando o treinamento e salvando os neuronios treinados em uma variavel
-var neuronios_treinados = neural.treinar([a,b,c], dados_treino, 10, 30);
-
-// testando ornitorrinco com os neuronios ja treinados
-console.log(neural.executar(neuronios_treinados, [1, 1, 0, 0, 0, 0]) ? 'ave' : 'nao é ave');
+module.exports = neural;
